1f2f92c74ad779da5b0c1f22c1b8e8e199a9d289
Carrie Mathieu <carrieburks@gmail.com>
2020-08-08 19:29:20 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,8 @@
 def speak_to_grandma(phrase)
   if phrase = "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN"
-  if phrase
+  if phrase == phrase.upcase
+    puts "NO, NOT SINCE 1938!"
 end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
